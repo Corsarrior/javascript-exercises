acba5f117a1180c385ec9634f19b21c673d407bd
Juan <juanjose1148@gmail.com>
2021-12-21 16:26:03 -0500

    Solution 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+    let stringRepeated = '';
+    if (repetitions < 0){
+        return 'ERROR';
+    }
+    else{
+        for (let i = 0; i < repetitions; i++)
+        {
+        stringRepeated = stringRepeated.concat(string)
+        }
+        return stringRepeated
+    }  
 };
 
 // Do not edit below this line
