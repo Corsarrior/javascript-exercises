67dc801c13400e76538600184212f64c3fb9c6e8
Juan <juanjose1148@gmail.com>
2021-12-22 22:10:12 -0500

    Solution 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let result = 0;
+    let i = start; 
+    if (typeof(end) != "number" || start < 0){
+        return "ERROR"
+    }
+    if (start < end){
+        while (i <= end){
+            result = result + i;
+            i++  
+        }
+    }
+    else{
+        while (i >= end){
+            result = result + i;
+            i--  
+        }
+    }
+    return result
 };
 
 // Do not edit below this line
